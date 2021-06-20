91da7f5beafafa341a0b6fd95f42edaed7dd4470
GreenAndEievui <14899090+GreenAndEievui@users.noreply.github.com>
2021-06-19 23:44:23 -0400

    Cleared project.

--- src/include/collision.h
+++
@@ -1,6 +0,0 @@
-typedef enum {
-	CLEAR_COLLISION,
-	WALL_COLLISION
-} COLLISION;
-
-COLLISION get_collision(uint16_t x, uint16_t y);
--- src/include/map.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-#include "vector.h"
-
-extern vec16 camera_position;
-
-void scroll_map(void);
